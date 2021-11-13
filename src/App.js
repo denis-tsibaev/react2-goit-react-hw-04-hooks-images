44484dd56a7bbba44b9c1184e5cda5833b7c6eeb
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-11-13 22:52:25 +0700

    class-> hooks refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import imageApi from './components/Api';
 import Button from './components/Button';
 import Container from './components/Container';
@@ -8,42 +8,36 @@ import Modal from './components/Modal';
 import Searchbar from './components/Searchbar';
 import Spinner from './components/Spinner';
 
-export default class App extends Component {
-    state = {
-        query: '',
-        hits: [],
-        currentPage: 1,
-        modal: false,
-        modalImage: '',
-        isLoading: false,
-        error: null,
+export default function App() {
+    const [query, setQuery] = useState('');
+    const [hits, setHits] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [modal, setModal] = useState(false);
+    const [modalImage, setModalImage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const option = { query, currentPage };
+
+    useEffect(() => {
+        fetchImg(option);
+    }, [option]);
+
+    const handleInputChange = query => {
+        setQuery(query);
+        // setQuery(query.trim());
+        setCurrentPage(1);
+        setHits([]);
     };
 
-    componentDidUpdate(prevProps, prevState) {
-        const { currentPage, query } = this.state;
-
-        if (prevState.query !== query) {
-            this.fetchImg();
-        }
-        if (prevState.currentPage !== currentPage) {
-            this.fetchImg(currentPage);
-        }
-    }
-
-    handleInputChange = data => {
-        this.setState({ query: data.trim(), currentPage: 1, hits: [] });
-    };
-
-    fetchImg = () => {
-        const { query, currentPage } = this.state;
-        const option = { query, currentPage };
+    const fetchImg = () => {
         if (!query) return;
 
-        this.setState({ isLoading: true });
+        setIsLoading(true);
 
         imageApi(option)
             .then(result => {
-                this.setState(prevState => ({
+                setHits(prevState => ({
                     hits: [...prevState.hits, ...mapper(result)],
                 }));
 
@@ -52,67 +46,167 @@ export default class App extends Component {
                     behavior: 'smooth',
                 });
             })
-            .catch(error => this.setState({ error }))
+            .catch(error => setError(error))
             .finally(() => {
-                this.setState({ isLoading: false });
+                setIsLoading(false);
             });
     };
 
-    loadMore = () => {
-        let { currentPage } = this.state;
+    const loadMore = () => {
+        let currentPage = currentPage;
         currentPage += 1;
-        this.setState({ currentPage });
+        setCurrentPage(currentPage);
     };
 
-    handleModalOpen = largeImage => {
-        this.setState({ modal: true, modalImage: largeImage });
+    const handleModalOpen = largeImage => {
+        setModal(true);
+        setModalImage(largeImage);
     };
 
-    handleModalEscape = e => {
-        if (e.keyCode === 27) this.resetModal();
+    const handleModalEscape = e => {
+        if (e.keyCode === 27) resetModal();
     };
 
-    handleBackdropClick = e => {
-        if (e.target === e.currentTarget) this.resetModal();
+    const handleBackdropClick = e => {
+        if (e.target === e.currentTarget) resetModal();
     };
 
-    resetModal = () => {
-        this.setState({ modal: false, modalImage: '' });
+    const resetModal = () => {
+        setModal(false);
+        setModalImage('');
         window.removeEventListener('keydown', this.handleModalEscape);
     };
 
-    render() {
-        const { hits, query, modal, modalImage, isLoading } = this.state;
-        const {
-            handleInputChange,
-            handleModalOpen,
-            handleModalEscape,
-            handleBackdropClick,
-            loadMore,
-        } = this;
+    return (
+        <Container>
+            <Searchbar onSubmit={handleInputChange} />
+            {query && (
+                <ImageGallery hits={hits} onImageClick={handleModalOpen} />
+            )}
 
-        return (
-            <Container>
-                <Searchbar onSubmit={handleInputChange} />
-                {query && (
-                    <ImageGallery hits={hits} onImageClick={handleModalOpen} />
-                )}
+            {isLoading && <Spinner />}
 
-                {isLoading && <Spinner />}
+            {hits.length > 0 && (
+                <Button onLoadClick={loadMore} text="Load more" />
+            )}
 
-                {hits.length > 0 && (
-                    <Button onLoadClick={loadMore} text="Load more" />
-                )}
-
-                {modal && (
-                    <Modal
-                        modalClose={handleModalEscape}
-                        handleBackdropClick={handleBackdropClick}
-                    >
-                        <img src={modalImage} alt="" />
-                    </Modal>
-                )}
-            </Container>
-        );
-    }
+            {modal && (
+                <Modal
+                    modalClose={handleModalEscape}
+                    handleBackdropClick={handleBackdropClick}
+                >
+                    <img src={modalImage} alt="" />
+                </Modal>
+            )}
+        </Container>
+    );
 }
+
+// export default class App extends Component {
+//     state = {
+//         query: '',
+//         hits: [],
+//         currentPage: 1,
+//         modal: false,
+//         modalImage: '',
+//         isLoading: false,
+//         error: null,
+//     };
+
+//     componentDidUpdate(prevProps, prevState) {
+//         const { currentPage, query } = this.state;
+
+//         if (prevState.query !== query) {
+//             this.fetchImg();
+//         }
+//         if (prevState.currentPage !== currentPage) {
+//             this.fetchImg(currentPage);
+//         }
+//     }
+
+//     handleInputChange = data => {
+//         this.setState({ query: data.trim(), currentPage: 1, hits: [] });
+//     };
+
+//     fetchImg = () => {
+//         const { query, currentPage } = this.state;
+//         const option = { query, currentPage };
+//         if (!query) return;
+
+//         this.setState({ isLoading: true });
+
+//         imageApi(option)
+//             .then(result => {
+//                 this.setState(prevState => ({
+//                     hits: [...prevState.hits, ...mapper(result)],
+//                 }));
+
+//                 window.scrollTo({
+//                     top: document.documentElement.scrollHeight,
+//                     behavior: 'smooth',
+//                 });
+//             })
+//             .catch(error => this.setState({ error }))
+//             .finally(() => {
+//                 this.setState({ isLoading: false });
+//             });
+//     };
+
+//     loadMore = () => {
+//         let { currentPage } = this.state;
+//         currentPage += 1;
+//         this.setState({ currentPage });
+//     };
+
+//     handleModalOpen = largeImage => {
+//         this.setState({ modal: true, modalImage: largeImage });
+//     };
+
+//     handleModalEscape = e => {
+//         if (e.keyCode === 27) this.resetModal();
+//     };
+
+//     handleBackdropClick = e => {
+//         if (e.target === e.currentTarget) this.resetModal();
+//     };
+
+//     resetModal = () => {
+//         this.setState({ modal: false, modalImage: '' });
+//         window.removeEventListener('keydown', this.handleModalEscape);
+//     };
+
+//     render() {
+//         const { hits, query, modal, modalImage, isLoading } = this.state;
+//         const {
+//             handleInputChange,
+//             handleModalOpen,
+//             handleModalEscape,
+//             handleBackdropClick,
+//             loadMore,
+//         } = this;
+
+//         return (
+//             <Container>
+//                 <Searchbar onSubmit={handleInputChange} />
+//                 {query && (
+//                     <ImageGallery hits={hits} onImageClick={handleModalOpen} />
+//                 )}
+
+//                 {isLoading && <Spinner />}
+
+//                 {hits.length > 0 && (
+//                     <Button onLoadClick={loadMore} text="Load more" />
+//                 )}
+
+//                 {modal && (
+//                     <Modal
+//                         modalClose={handleModalEscape}
+//                         handleBackdropClick={handleBackdropClick}
+//                     >
+//                         <img src={modalImage} alt="" />
+//                     </Modal>
+//                 )}
+//             </Container>
+//         );
+//     }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,44 +1,78 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import '../../styles.css';
 
-export default class Searchbar extends Component {
-    state = {
-        query: '',
+export default function Searchbar(props) {
+    const [query, setQuery] = useState('');
+
+    const handleChange = e => {
+        setQuery(e.target.value);
     };
 
-    handleChange = e => {
-        this.setState({ query: e.target.value });
-    };
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-        this.props.onSubmit(this.state.query);
-        this.setState({ query: '' });
+        this.props.onSubmit(query);
+        setQuery('');
     };
 
-    render() {
-        return (
-            <header className="Searchbar">
-                <form className="SearchForm" onSubmit={this.handleSubmit}>
-                    <button type="submit" className="SearchForm-button">
-                        <span className="SearchForm-button-label">Search</span>
-                    </button>
+    return (
+        <header className="Searchbar">
+            <form className="SearchForm" onSubmit={handleSubmit}>
+                <button type="submit" className="SearchForm-button">
+                    <span className="SearchForm-button-label">Search</span>
+                </button>
 
-                    <input
-                        className="SearchForm-input"
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        value={this.state.query}
-                        placeholder="Search images and photos"
-                        onChange={this.handleChange}
-                    />
-                </form>
-            </header>
-        );
-    }
-    static propTypes = {
-        handleSubmit: PropTypes.func,
-    };
+                <input
+                    className="SearchForm-input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    value={query}
+                    placeholder="Search images and photos"
+                    onChange={handleChange}
+                />
+            </form>
+        </header>
+    );
 }
+
+// export default class Searchbar extends Component {
+//     state = {
+//         query: '',
+//     };
+
+//     handleChange = e => {
+//         this.setState({ query: e.target.value });
+//     };
+
+//     handleSubmit = e => {
+//         e.preventDefault();
+//         this.props.onSubmit(this.state.query);
+//         this.setState({ query: '' });
+//     };
+
+//     render() {
+//         return (
+//             <header className="Searchbar">
+//                 <form className="SearchForm" onSubmit={this.handleSubmit}>
+//                     <button type="submit" className="SearchForm-button">
+//                         <span className="SearchForm-button-label">Search</span>
+//                     </button>
+
+//                     <input
+//                         className="SearchForm-input"
+//                         type="text"
+//                         autoComplete="off"
+//                         autoFocus
+//                         value={this.state.query}
+//                         placeholder="Search images and photos"
+//                         onChange={this.handleChange}
+//                     />
+//                 </form>
+//             </header>
+//         );
+//     }
+//     static propTypes = {
+//         handleSubmit: PropTypes.func,
+//     };
+// }
